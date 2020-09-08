12a864aff189f09a40773707d171b22eba61c43d
SUCHMOKUO <suchmokuo@gmail.com>
2020-09-08 23:34:43 +0800

    finish exercise 4

--- src/exercise_04/arrayToList.js
+++ src/exercise_04/arrayToList.js
@@ -1,3 +1,10 @@
+class Node {
+  constructor(value, next) {
+    this.value = value;
+    this.next = next;
+  }
+}
+
 export default function arrayToList(array) {
   // This function creates a linked list from an array.
   //
@@ -6,5 +13,22 @@ export default function arrayToList(array) {
   // * Please implement this function and pass all the tests in array_to_list_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  if (!array) {
+    throw new Error('Creating list from undefined array');
+  }
+
+  if (!array.length) {
+    throw new Error('Creating list from empty array');
+  }
+
+  const head = new Node(array[0], null);
+  let currentNode = head;
+
+  for (let i = 1; i < array.length; i += 1) {
+    const nextNode = new Node(array[i], null);
+    currentNode.next = nextNode;
+    currentNode = nextNode;
+  }
+
+  return head;
 }
