f9c83dfbcba9ac8e239638288f060ab42b721eb0
SUCHMOKUO <suchmokuo@gmail.com>
2020-09-08 23:26:20 +0800

    finish exercise 3

--- src/exercise_03/rangeAndSum.js
+++ src/exercise_03/rangeAndSum.js
@@ -7,7 +7,19 @@ export function range(start, end) {
   // * Please implement this function and pass all the tests in range_and_array_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  const result = [];
+
+  if (start < end) {
+    for (let i = start; i < end; i += 1) {
+      result.push(i);
+    }
+  } else if (start > end) {
+    for (let i = start; i > end; i -= 1) {
+      result.push(i);
+    }
+  }
+
+  return result;
 }
 
 export function sum(...numbers) {
@@ -18,5 +30,5 @@ export function sum(...numbers) {
   // * Please implement this function and pass all the tests in range_and_array_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  return numbers.reduce((result, number) => result + number, 0);
 }
